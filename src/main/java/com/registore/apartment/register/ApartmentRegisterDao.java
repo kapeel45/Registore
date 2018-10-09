721ddda688751635320d91de378b9e68d198b790
Pihu115 <singhpunita11@gmail.com>
2018-10-09 14:56:19 +0530

    error in code

--- src/main/java/com/registore/apartment/register/ApartmentRegisterDao.java
+++ src/main/java/com/registore/apartment/register/ApartmentRegisterDao.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ApartmentRegisterDao extends CrudRepository<ApartmentRegister, Long> {
 
-	@Query("select * from apartment_register ar where ar.apartment_name = ?1")
+	@Query("select ar from apartment_register ar where ar.apartment_name = ?1")
 	ApartmentRegister findByApartmentName(String apartmentName);
 	
 }
